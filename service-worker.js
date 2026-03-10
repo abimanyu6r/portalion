36243560bb09277c88e246b4e10966390a3ecb55
Abimanyu Ridho <abimanyu6r@gmail.com>
2026-03-10 13:12:03 +0700

    Make the web installable

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "my-app-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/portal.html",
+
+  // images
+  "/foto/Landing Page.png",
+  "/foto/Open source-rafiki 1.png",
+  "/foto/upa-pkk-logo 1.png",
+  "/foto/upa-pkk-logo 512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
